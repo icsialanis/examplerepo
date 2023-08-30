bfc75e6305a1c1c3230acb067bb235562efbacd4
icsialanis <alanis.carpio.07@gmail.com>
2023-08-30 14:01:06 -0700

    Update ExamplePython.py
    added job list

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,3 +1,7 @@
 a = "hello world"
 b = "whats up"
 print(f"a = {a} and b = {b}")
+job_list = {'jhon': 'doctor', 'jane': 'engineer', 'jim': 'teacher'}
+
+for name, job in job_list.items():
+    print(f"{name} is a {job}")
